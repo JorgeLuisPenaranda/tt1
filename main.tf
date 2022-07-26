20df4bc3e4bf573d80a56bb4891be517a1657b62
JorgeLuisPenaranda <jorge.penaranda.arandia@gmail.com>
2022-07-26 02:40:23 -0700

    Deploy test 30

--- main.tf
+++ main.tf
@@ -54,6 +54,7 @@ resource "aws_instance" "web" {
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
               echo "Hello World" > /var/www/html/index.html
+              chmod -R 777 /var/www/html/
               systemctl restart apache2
               EOF
 }
