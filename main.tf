07fa8a70cddf53c312e7ef2284d518eb4889242b
JorgeLuisPenaranda <jorge.penaranda.arandia@gmail.com>
2022-07-26 01:42:13 -0700

    Deploy test 25

--- main.tf
+++ main.tf
@@ -75,6 +75,7 @@ resource "aws_security_group" "web-sg" {
 }
 
 output "web-address" {
+  value = "${aws_instance.web.public_dns}"
   //value = "${aws_instance.web.public_dns}:8080"
-  value = "${aws_eip.bastion.public_ip}"
+  //value = "${aws_eip.bastion.public_ip}"
 }
