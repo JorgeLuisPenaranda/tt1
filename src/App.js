e1bcba5bffa63430250ee9f7d488788337bb6d27
JorgeLuisPenaranda <jorge.penaranda.arandia@gmail.com>
2022-07-26 04:54:36 -0700

    Adding stage env

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Calculator DEV ENV testing</div>
+        <div>CIDR Calculator STAGING ENV</div>
       </header>
       <IPv4Addr />
       <footer>
